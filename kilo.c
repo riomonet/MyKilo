bfe50f17862aac26ea98bd56c59a5c1eff033584
Ari Zablozki <ari@marina59.com>
2026-02-23 17:31:12 -0500

    centered title

--- kilo.c
+++ kilo.c
@@ -15,11 +15,12 @@
 /* 0x00011111 & key */
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define ABUF_INIT {NULL, 0}
-
+#define KILO_VERSION "0.0.1"
 
 /* DATA */
 struct editorConfig
 {
+    int cx,cy;
     int screenrows;
     int screencols;
     termios orig_termios;
@@ -32,6 +33,7 @@ struct abuf
 };
 
 editorConfig E;
+
 
 /* TERMINAL */
 void die(const char *s)
@@ -161,7 +163,35 @@ void editorDrawRows(abuf *ab)
     int y;
     for (y = 0; y < E.screenrows; y++)
     {
-	abAppend(ab, "~", 1);
+	if(y == E.screenrows / 3)
+	{
+	    char welcome[80];
+	    int welcomelen = snprintf(welcome, sizeof(welcome),
+				      "kilo editor --version %s", KILO_VERSION);
+	    if (welcomelen > E.screencols)
+		{
+		welcomelen = E.screencols;
+		}
+	    
+	    int padding = (E.screencols - welcomelen) / 2;
+
+	    if(padding)
+	    {
+		abAppend(ab, "~", 1);
+		padding--;
+	    }
+	    
+	    while(padding--) 
+	    {
+		abAppend(ab," ",1);
+	    }
+	    abAppend(ab,welcome, welcomelen);
+	}
+	    else
+	{
+	    abAppend(ab, "~", 1);
+	}
+	
 	abAppend(ab, "\x1b[K", 3); // clears rest of line
 	// 2 erases whole line
 	// 1 erases to the left of the cursor
@@ -213,12 +243,16 @@ void editorProcessKeypress()
 /* INIT */
 void initEditor()
 {
+    E.cx = 0;
+    E.cy = 0;
+    
     if (getWindowSize(&E.screenrows, &E.screencols) == -1)
     {
         die("getWindowsSize");
     }
  
 }
+
 
 int main()
 {
