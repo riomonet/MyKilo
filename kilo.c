797b274bddee7f15efd07e8d11bafe66d7429a62
Ari Zablozki <ari@marina59.com>
2026-02-23 21:01:37 -0500

    pg_up pg_dwn home, end

--- kilo.c
+++ kilo.c
@@ -19,13 +19,16 @@ void editorMoveCursor(int key);
 #define KILO_VERSION "0.0.1"
 
 
-
 enum editorkey
 {
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
-    ARROW_DOWN
+    ARROW_DOWN,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN
 };
 
     
@@ -118,15 +121,46 @@ int editorReadKey()
 
 	if (seq[0] == '[')
 	{
-	    switch (seq[1])
+	    if (seq[1] >= '0' && seq[1] <= '9')
+	    {		
+		if (read(STDIN_FILENO, &seq[2], 1) != 1)
+		{
+		    return '\x1b';
+		}
+		if (seq[2] == '~')
+		{
+		    switch(seq[1])
+		    {
+			case '1' : return HOME_KEY;
+			case '4' : return END_KEY;
+			case '5' : return PAGE_UP;
+			case '6' : return PAGE_DOWN;
+			case '7' : return HOME_KEY;
+			case '8' : return END_KEY;
+		    }
+		}
+	    }
+	    else
 	    {
-		case 'A': return  ARROW_UP;
-		case 'B': return  ARROW_DOWN;
-		case 'C': return  ARROW_RIGHT;
-		case 'D': return  ARROW_LEFT;
+		switch (seq[1])
+		{
+		    case 'A': return  ARROW_UP;
+		    case 'B': return  ARROW_DOWN;
+		    case 'C': return  ARROW_RIGHT;
+		    case 'D': return  ARROW_LEFT;
+		    case 'E': return  HOME_KEY;
+		    case 'F': return  END_KEY;
+		}
 	    }
 	}
-	
+	else if(seq[0] == 'O')
+	{
+	    switch(seq[1])
+	    {
+		case 'H': return HOME_KEY;
+		case 'F': return END_KEY;
+	    }
+	}
 	return '\x1b';	
     }
     else
@@ -257,6 +291,24 @@ void editorRefreshScreen()
 
     editorDrawRows(&ab);
 
+    
+    if (E.cy < 0)
+    {
+	E.cy = 0;
+    }
+    if(E.cy > E.screenrows - 1)
+    {	
+	E.cy = E.screenrows;
+    }
+    if(E.cx < 0)
+    {
+	E.cx = 0;
+    }
+    if (E.cx > E.screencols)
+    {
+	E.cx = E.screencols;
+    }
+    
     char buf[32] = {};
     snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
     abAppend(&ab, buf, strlen(buf));
@@ -280,6 +332,24 @@ void editorProcessKeypress()
             write(STDOUT_FILENO, "\x1b[2J", 4);
             exit(0);
         } break;
+
+	case HOME_KEY:
+	{
+	    E.cx = 0;
+	} break;
+
+	case END_KEY:
+	{
+	    E.cx = E.screencols - 1;
+	} break;
+
+	case PAGE_UP:
+	case PAGE_DOWN:
+	{
+	    int times = E.screenrows;
+	    while (times--)
+		editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+	} break;
 
 	case ARROW_UP:
 	case ARROW_DOWN:
